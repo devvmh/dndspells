b13358ccd9b10db85df775abd5aecf6a08e98ee8
Devin Howard <devin@callysto.com>
2016-03-31 14:50:46 +0800

    set up js

---
+++ js/src/SpellEditor.js
@@ -0,0 +1,60 @@
+import React, { Component, PropTypes } from 'react'
+import _ from 'lodash'
+
+class SpellEditor extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentClass: null,
+      spells: props.spells
+    }
+  }
+
+  chooseClass = newClass => {
+    this.setState({
+      ...state,
+      currentClass: newClass
+    })
+  }
+
+  assignSpell = spell => {
+    alert("TODO")
+  }
+
+  organizeSpells = () => {
+    return this.state.spells
+  }
+
+  render = () => {
+    <div>
+      <ClassChooser onChange={this.chooseClass}
+        currentClass={this.state.class}
+      />
+      <ClassSpellAssigner onAssign={this.assignSpell} 
+        currentClass={this.state.class}
+        spells={this.organizeSpells()}
+      />
+    </div>
+  }
+}
+
+const ClassSpellAssigner = (props) => (
+  <div>
+    <p>Here are all of the spells:</p>
+    <ul>
+      {props.spells.map(spell => (
+        <li key={spell.id}>{spell.name}</li>
+      ))}
+    </ul>
+  </div>
+)
+
+const ClassChooser = (props) => (
+  <label>
+    Which class do you want?
+    <input type="text"
+      value={this.props.currentClass}
+      onChange={this.props.onChange}
+    />
+  </label>
+)
---
+++ js/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import SpellEditor from './SpellEditor'
+
+ReactDOM.render(
+  <SpellEditor spells={spells} />,
+  document.getElementById('spell-editor')
+)
