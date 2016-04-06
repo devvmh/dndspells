e8877653b678049e01ca0f948ecefd06a579edc0
Devin Howard <devin@callysto.com>
2016-04-06 10:15:30 +0800

    order spells, change index

--- spells/views.py
+++ spells/views.py
@@ -6,14 +6,19 @@ from .models import *
 
 def index(request):
   return render(request, 'index.html', {
-    'spells': json.dumps([x for x in Spell.objects.values()])
+    'spells': json.dumps([x for x in spells()]),
+    'classes': json.dumps([x for x in CasterClass.objects.values()])
   })
 
 # private methods
 
+def get_classes(spell):
+  return list(map(lambda x: x['name'], Spell.objects.get(id=spell['id']).classes.values()))
+
+def with_classes(spell):
+  spell['classes'] = get_classes(spell)
+  return spell
+
 def spells():
-  spells = {}
-  for spell in Spell.objects.values():
-    spells[spell['id']] = spell
+  spells = map(with_classes, Spell.objects.order_by('name').values())
   return spells
-  
