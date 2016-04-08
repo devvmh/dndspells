a052c3d567989856586fc40acfd021a8e4f5f425
Devin Howard <devin@callysto.com>
2016-04-08 10:31:52 +0800

    passenger_wsgi with localconfig options

---
+++ dndspells/passenger_wsgi.py
@@ -0,0 +1,22 @@
+# Set up the virtual environment:
+import os, sys
+try:
+  from localconfig import PASSENGER_PATH
+  from localconfig import PASSENGER_VIRTUAL_ENV
+  from localconfig import PASSENGER_CHDIR
+except:
+  PASSENGER_PATH = '/home/dnd/dndspells/venv/bin:/usr/local/bin:/usr/bin'
+  PASSENGER_VIRTUAL_ENV = '/home/dnd/dndspells/venv/bin'
+  PASSENGER_CHDIR = '/home/dnd/dndspells'
+
+os.environ['PATH'] = PASSENGER_PATH
+os.environ['VIRTUAL_ENV'] = PASSENGER_VIRTUAL_ENV
+os.environ['PYTHON_EGG_CACHE'] = PASSENGER_VIRTUAL_ENV
+os.chdir(PASSENGER_CHDIR)
+sys.path.insert(0, PASSENGER_CHDIR)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'dndspells.settings'
+
+# only for Django >= 1.7
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
