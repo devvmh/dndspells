3dbf6e0f9e5f9d636253c021b118716ef6525a16
Devin Howard <devin@callysto.com>
2016-04-08 11:26:37 +0800

    fix permissions script

---
+++ bin/fix-permissions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DEPLOY_DIR=/home/dnd/dndspells
+DEPLOY_USER=dnd
+WEB_USER=www-data
+
+set -e
+
+#default is locked down from web access
+chown -R ${DEPLOY_USER}:${DEPLOY_USER} ${DEPLOY_DIR}
+find ${DEPLOY_DIR} -type d -exec chmod u=rwx,g=rx,o= '{}' \;
+find ${DEPLOY_DIR} -type f -exec chmod u=rw,g=r,o= '{}' \;
+
+#allow web server to access web directory
+chgrp -R ${WEB_USER} ${DEPLOY_DIR}/dndspells
+
+#venv setup
+chgrp -R ${WEB_USER} ${DEPLOY_DIR}/venv/bin
+chmod -R g-w ${DEPLOY_DIR}/venv/bin
+
+#allow editing sqlite file
+chmod g+w ${DEPLOY_DIR}/db.sqlite3
+
+#executables
+chmod -R ug+x ${DEPLOY_DIR}/venv/bin
+chmod -R ug+x ${DEPLOY_DIR}/
+chmod -R u+x ${DEPLOY_DIR}/bin
--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(basename $0)
+cd $(dirname $0)/..
 source venv/bin/activate
 
 python manage.py migrate
@@ -9,6 +9,7 @@ cd js
 npm run build
 cd ..
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
+bin/fix-permissions.sh
 
 passenger-config restart-app .
