ef395a53b6e3b8a6af881673599b74bb7a56669d
Devin Howard <devin@callysto.com>
2016-04-19 17:19:08 +0800

    tweak script

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -3,15 +3,15 @@
 cd $(dirname $0)/..
 source venv/bin/activate
 
-if [[ "$1" == "-p" ]]; then
-  git pull
-fi
+git pull
 
 python manage.py migrate
 
 (cd js && npm run build)
 
 python manage.py collectstatic --noinput
+
+echo "Fixing perms..."
 bin/fix-permissions.sh
 
 passenger-config restart-app .
