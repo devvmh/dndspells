eac90289933469b5072199fce209bbcbc47eba8c
Devin Howard <devin@callysto.com>
2016-05-19 09:48:25 +0800

    spellbook works, but not react-typeahead. works for me!

--- js/src/MySpellBook.js
+++ js/src/MySpellBook.js
@@ -23,21 +23,34 @@ class MySpellBook extends Component {
   }
 
   handleChangeFiltererState = filtererState => {
-    this.props.changeTabState({ filtererState })
+    const newState = this.props.state
+    newState.filtererState = filtererState
+    this.props.changeTabState(newState)
   }
 
-  handleSpellbookAdd = option => {
-    console.log(option)
+  handleSpellbookAdd = e => {
+    const spellbook = this.props.state.spellbook
+    const spell = this.props.spells.filter(item => item.id == e.target.value).pop()
+    spellbook.push(spell)
+    const newState = this.props.state
+    newState.spellbook = spellbook
+    this.props.changeTabState(newState)
   }
 
-  handleSpellbookRemove = option => {
-    console.log(option)
+  handleSpellbookRemove = e => {
+    const spellbook = this.props.state.spellbook
+    const spell_id = e.target.value
+    const newState = this.props.state
+    newState.spellbook = spellbook.filter(spell => spell.id != spell_id)
+    this.props.changeTabState(newState)
   }
 
   handleBackupValueChange = e => {
     try {
       const spellbook = JSON.parse(e.target.value)
-      this.props.changeTabState({ spellbook })
+      const newState = this.props.state
+      newState.spellbook = spellbook
+      this.props.changeTabState(newState)
     } catch (error) {
       alert("Backup data was invalid :(")
       console.error(error)
@@ -48,12 +61,16 @@ class MySpellBook extends Component {
     return (
       <div>
         <p>To backup your spellbook, copy this text to a file. You can paste it here to restore the backup later.</p>
-        <textarea onChange={this.handleBackupValueChange} >
-          {JSON.stringify(this.props.state.spellbook)}
-        </textarea>
+        <textarea onChange={this.handleBackupValueChange}
+          value={JSON.stringify(this.props.state.spellbook)}
+        />
       </div>
     )
   }
+
+  // <Typeahead onOptionSelected={this.handleSpellbookAdd}
+  //   options={this.props.spells.map(spell => spell.name)}
+  // />
 
   render = () => {
     if (!this.props.state) {
@@ -61,9 +78,18 @@ class MySpellBook extends Component {
     }
     return (
       <div>
-        <Typeahead onOptionSelected={this.handleSpellbookAdd}
-          options={this.props.spells.map(spell => spell.name)}
-        />
+        <select onChange={this.handleSpellbookAdd}>
+          <option>-- Add spell --</option>
+          {this.props.spells.map(spell => (
+            <option key={spell.id} value={spell.id}>{spell.name}</option>
+          ))}
+        </select>
+        <select onChange={this.handleSpellbookRemove}>
+          <option>-- Remove spell --</option>
+          {this.props.state.spellbook.map(spell => (
+            <option key={spell.id} value={spell.id}>{spell.name}</option>
+          ))}
+        </select>
         <SpellFilterer spells={this.props.state.spellbook || []}
           classes={this.props.classes}
           authenticated={this.props.authenticated}
